4d82170b1dcf77ed166094222f32e59b180a4c3a
awaiss400 <testingbscs@gmail.com>
2022-09-06 21:31:47 +0500

    Revert "Revert "its second comiit with error""
    This reverts commit 07990c11425a6606036c2a67983b74be789035ec.

--- app/src/main/java/com/stone/nothing/MainActivity.kt
+++ app/src/main/java/com/stone/nothing/MainActivity.kt
@@ -22,8 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    cysdc
+                    Greeting("First commit ever")
+                    this is error
                 }
             }
         }
